2b710f8efd584ec6cb5494f724fb38c06cc74687
Mohamed Darwesh <mohameddrawesh2000@gmail.com>
2024-06-21 17:27:29 +0300

    Removed unused timestamp attribute

--- Back/src/models/Attribute.php
+++ Back/src/models/Attribute.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 class Attribute extends Model {
     protected $table = 'attributes';
 
-    public $timestamps = false;
-
     public function product() {
         return $this->belongsTo(Product::class);
     }
