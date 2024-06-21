739ea74caa033bac910a4540d61e323d084ada45
Mohamed Darwesh <mohameddrawesh2000@gmail.com>
2024-06-21 19:13:26 +0300

    Created an abstract class for common attributes

---
+++ Back/src/models/BaseAttribute.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseAttribute extends Model
+{
+    protected $table;
+    public function product()
+    {
+    }
+}
