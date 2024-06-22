4aff2e6ae6360f241bd1002ae58738fa2c889a70
Mohamed Darwesh <mohameddrawesh2000@gmail.com>
2024-06-22 19:23:34 +0300

    Added route for Product Detail Page

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import PublicLayout from './layouts/PublicLayout';
+import ProductDetailPage from './pages/ProductDetailPage/ProductDetailPage';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<PublicLayout><Navigate to="/all" /></PublicLayout>} />
         <Route path="/:category" element={<PublicLayout><HomePage /></PublicLayout>} />
+        <Route path="/product/:productId" element={<PublicLayout><ProductDetailPage /></PublicLayout>} />
       </Routes>
     </Router>
   );
