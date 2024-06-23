886ab3850beeafdecd4471b0b34993bd28b94be9
Mohamed Darwesh <mohameddrawesh2000@gmail.com>
2024-06-24 00:29:49 +0300

    redeploy

--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -3,13 +3,6 @@ import App from './App';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import CartProvider from './context/CartContext';
 
-await fetch("https://api.vercel.com/v1/projects/prj_XWlmjYRKOvr5EXWlm6baqZx87pOP/env/p0REZjrfmLsnI4rA", {
-  "headers": {
-    "Authorization": "Bearer 74jBTzaodoKfRIHoBmcFA7iI"
-  },
-  "method": "get"
-})
-
 const apiURL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 console.log("API URL:", apiURL);
 const client = new ApolloClient({
